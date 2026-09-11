814bd69f28271eb4193e66a0986cbadc9374a636
SewoShun <93058265+SewoShun@users.noreply.github.com>
2026-09-11 10:21:47 +0900

    modify fonts config

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -2,7 +2,12 @@
 delib.module {
   name = "fonts";
 
+  nixos.always.fonts.fontconfig.enable = true;
   nixos.always.fonts.packages = with pkgs; [
+    dejavu_fonts
+    liberation_ttf
+    noto-fonts
+    noto-fonts-cjk-sans
     ipaexfont
     migu
     nerd-fonts.zed-mono
